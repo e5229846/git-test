e36803f07836439ad14353bc1c2fd9c2be847503
e5229846 <58823815+e5229846@users.noreply.github.com>
2020-07-16 15:24:04 +0800

    v9.0

--- src/Tests.java
+++ src/Tests.java
@@ -6,8 +6,5 @@ public class Tests {
         System.out.println("仓库");
         System.out.println("v4.0");
         System.out.println("hasune");
-        System.out.println("分支");
-        System.out.println("分支2");
-        System.out.println("分支3");
     }
 }
