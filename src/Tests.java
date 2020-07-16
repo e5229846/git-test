dee65fd90d2a617d5fb64c273f33befbba762204
e5229846 <290240329@qq.com>
2020-07-16 13:54:28 +0800

    v4.0

--- src/Tests.java
+++ src/Tests.java
@@ -4,5 +4,6 @@ public class Tests {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("仓库");
+        System.out.println("v4.0");
     }
 }
